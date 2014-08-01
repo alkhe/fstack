ac7901be747934cc5f851db844d86d7c3e2790ff
chronize <sort@programmer.net>
2014-08-01 14:20:25 -0400

    implement small core features

--- fstack.js
+++ fstack.js
@@ -1,12 +1,38 @@
 (function(fstack) {
 
 	var fs = require('fs'),
-		path = require('path');
+		path = require('path'),
+		async = require('async'),
+		_ = require('lodash');
 
 	fstack = {
-		entsl: 
-		dirsl: 
-		filesl: 
+		entsx: function(path, callback) {
+			this.ents(path, function(err, ents) {
+				if (err)
+					return callback(err);
+				async.map(ents, fs.stat, function(err, stats) {
+					callback(err, {
+						ents: ents,
+						stats: _.zipObject(ents, stats)
+					});
+				});
+			});
+		},
+		ents: function(path, callback) {
+			fs.readdir(path, callback);
+		},
+		dirs: function(path, callback) {
+
+		},
+		files: function(path, callback) {
+			this.entsx(path, function(err, data) {
+				if (err)
+					return callback(err);
+				callback(err, _.filter(data.stats, function(ent, stat) {
+					return stat.isFile();
+				});
+			});
+		},
 	}
 
 })(module.exports);
