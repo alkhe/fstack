1dfdd3782f81d4826719d14965a09a1d396d6cc6
chronize <sort@programmer.net>
2014-08-08 14:55:53 -0400

    lodash fixes

--- fstack.js
+++ fstack.js
@@ -33,6 +33,8 @@
 			'string': false,
 			'undefined': false
 		},
+		objectProto = Object.prototype,
+		toString = objectProto.toString,
 		reNative = new RegExp('^' +
 			String(toString)
 			.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
@@ -65,11 +67,56 @@
 				return [];
 			}
 			return nativeKeys(object);
+		},
+		baseCreateCallback = function baseCreateCallback(func, thisArg, argCount) {
+			if (typeof func !== 'function') {
+				return identity;
+			}
+			if (typeof thisArg === 'undefined' || !('prototype' in func)) {
+				return func;
+			}
+			var bindData = func.__bindData__;
+			if (typeof bindData === 'undefined') {
+				if (support.funcNames) {
+					bindData = !func.name;
+				}
+				bindData = bindData || !support.funcDecomp;
+				if (!bindData) {
+					var source = fnToString.call(func);
+					if (!support.funcNames) {
+					bindData = !reFuncName.test(source);
+					}
+					if (!bindData) {
+						bindData = reThis.test(source);
+						setBindData(func, bindData);
+					}
+				}
+			}
+			if (bindData === false || (bindData !== true && bindData[1] & 1)) {
+				return func;
+			}
+			switch (argCount) {
+				case 1: return function(value) {
+					return func.call(thisArg, value);
+				};
+				case 2: return function(a, b) {
+					return func.call(thisArg, a, b);
+				};
+				case 3: return function(value, index, collection) {
+					return func.call(thisArg, value, index, collection);
+				};
+				case 4: return function(accumulator, value, index, collection) {
+					return func.call(thisArg, accumulator, value, index, collection);
+				};
+			}
+			return bind(func, thisArg);
 		};
 
 	(function(object, source, guard) {
 		var index, iterable = object, result = iterable;
-		if (!iterable) return result;
+		if (!iterable) {
+			return result;
+		}
 		var args = arguments,
 		argsIndex = 0,
 		callback,
